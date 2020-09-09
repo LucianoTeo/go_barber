ada814bec949b6c88697194e94ec39c2853c6712
LucianoTeo <lucianojunioteodoro@gmail.com>
2020-09-09 13:54:07 +0100

    continuing bootcamp module 4

---
+++ backend/src/modules/users/infra/http/controllers/SessionsController.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from 'express';
+
+import AuthenticationUserService from '@modules/users/services/AuthenticationUserService';
+import { container } from 'tsyringe';
+
+class SessionsController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { email, password } = request.body;
+
+    const authenticateUser = container.resolve(AuthenticationUserService);
+
+    const { user, token } = await authenticateUser.execute({
+      email,
+      password,
+    });
+
+    delete user.password;
+
+    return response.json({ user, token });
+  }
+}
+
+export default SessionsController;
